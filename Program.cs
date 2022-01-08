cbf6c6ed2792dce50f11ddbae2071a0bbaf75c05
Thiago Lanza <lanzathm@gmail.com>
2022-01-08 13:11:05 -0300

    Basic example of Post verb and getting params from route and body

--- Program.cs
+++ Program.cs
@@ -3,4 +3,25 @@ var app = builder.Build();
 
 app.MapGet("/", () => new { message = "Hello World!" });
 
+app.MapGet("/name/{name}", (string name) =>
+{
+    // {name} é um query param, retorna um JSON com status code 200 (Ok).
+    return Results.Ok(new { message = $"Hello {name}" });
+});
+
+app.MapPost("/", (User user) =>
+{
+    return Results.Created("/", user);
+});
+
 app.Run();
+
+public class User
+{
+    // Constructor gera o Id.
+    public User() => Id = Guid.NewGuid();
+
+    public Guid Id { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+}
